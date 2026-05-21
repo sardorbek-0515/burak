150ce816a1f98a9e55bde324204d14cdc94b7edf
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-05-22 02:11:56 +0900

    fix: Mit ASk-L

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,10 @@
-console.log("hello world")
+//Mitask------L
+function reverseSentence(sentence: string): string {
+  return sentence
+    .split(" ") 
+    .map(word => word.split("").reverse().join("")) 
+    .join(" "); 
+}
+
+
+console.log(reverseSentence("we like coding!"));
