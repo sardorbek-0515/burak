ffd726cf8c8012997d9ad79ced8f6f20b872cfce
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-05-25 13:20:26 +0900

    fix: Mit Ask-M

--- src/train.ts
+++ src/train.ts
@@ -6,5 +6,18 @@ function reverseSentence(sentence: string): string {
     .join(" "); 
 }
 
-
 console.log(reverseSentence("we like coding!"));
+
+
+
+//Mitask------M
+function getSquareNumbers(arr: number[]): { number: number; square: number }[] {
+  return arr.map((num: number) => ({
+    number: num,
+    square: num * num
+  }));
+}
+
+
+console.log(getSquareNumbers([1, 2, 3]));
+
