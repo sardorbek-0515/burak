70047547960599998cc5d9e6763daf5233c448f1
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-05-30 11:04:55 +0900

    fix:

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 /** 4-ROUTERS   **/
-app.use("/admin", routerAdmin);    // SSR: EJS 
-app.use("/", router);              // SPA: REACT
+app.use("/admin", routerAdmin);    // SSR: EJS togridan togri
+app.use("/", router);              // SPA: REACT rest API orqali7
 
 export default app; //module.exports 
