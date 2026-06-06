61da8ecd033c81184ad363c4c66f53b4bd66a25c
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-06-06 22:46:36 +0900

    feat: intergrate middleware  session

--- src/train.ts
+++ src/train.ts
@@ -134,6 +134,5 @@ function hasProperty(obj: object, key: string): boolean {
     return key in obj;
 }
 
-// Test
 console.log(hasProperty({name: "BMW", model: "M3"}, "model")); // true
 console.log(hasProperty({name: "BMW", model: "M3"}, "year"));  // false
