b89e8946cf1aa9d6f2a65d15a0ddb2361c860dde
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-06-13 18:50:00 +0900

    feat: createdNewProduct business logic by restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from "express";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/comman";
 import ProductServer from "../models/Product.service";
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 
 
 const productService = new ProductServer
 
 //getAllProducts
 const productController: T = {};
+/**  SPA */
+
+/**  SRR */
+//adminka loyhasi uchun
+//getAllProducts
 productController.getAllProducts = async (req: Request, res: Response) => {
     try {
         console.log("getAllProducts")
@@ -21,16 +28,27 @@ productController.getAllProducts = async (req: Request, res: Response) => {
 };
 
 //createNewProduct
-productController.createNewProduct = async (req: Request, res: Response) => {
+productController.createNewProduct = async (
+    req: AdminRequest, 
+    res: Response
+) => {
     try {
         console.log("createNewProduct")
-        const file = req.files;
-        console.log(file);
-        res.send("DONE!");
+        if(!req.files?.length) //bu array length 0dan kotta bolishi kerak
+         throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED );
+ 
+        const data: ProductInput = req.body;
+        data.productImages = req.files?.map(ele => {
+            return ele.path.replace(/\\/g, "/");
+        });
+
+        await productService.createNewProduct(data)
+            console.log("data", data)  
+        res.send(`<script> alert("Sucessful creation!"); window.location.replace('/admin/product/all') </script>`);
     } catch (err) {
         console.log("Error, createNewProduct:", err);
-        if (err instanceof Errors) res.status(err.code).json(err)
-        else res .status(Errors.standard.code).json(Errors.standard);
+        const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+       res.send(`<script> alert("${message}"); window.location.replace('/admin/product/all') </script>`);
 
     }
 };
