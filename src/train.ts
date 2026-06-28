5bed0327718022c1b1454df003231e3efe289c1b
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-06-28 18:39:57 +0900

    fix MIT ASK- Y

--- src/train.ts
+++ src/train.ts
@@ -270,4 +270,15 @@ const data = {
   }
 };
 
-console.log(countOccurrences(data, 'model')); 
+console.log(countOccurrences(data, 'model'));
+
+///////////// Y-TASK ///////////
+
+//Shunday function yozing, uni 2 ta array parapetri bolsin. Function ikkala arrayda ham ishtirok etgan qiymatlarni bir arrayda qaytarsin. MASALAN: findIntersection([1,2,3], [3,2,0]) return [2,3].
+
+function findIntersection(arr1: number[], arr2: number[]): number[] {
+  return arr1.filter((item) => arr2.includes(item));
+}
+
+
+console.log(findIntersection([1, 2, 3], [3, 2, 0])); 
